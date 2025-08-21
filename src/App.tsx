cf3bb326951d43a3c22e932559b86c8405251365
Zolu site <admin@zolurecruitment.com>
2025-08-21 16:09:56 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { NavLink, Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
