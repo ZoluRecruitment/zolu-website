55fbe8a2bbdb1ae7cf3717b6e7592c319338ecfd
Zolu site <admin@zolurecruitment.com>
2025-09-03 11:38:55 +1000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,50 @@
 import { NavLink } from "react-router-dom";
-import "./header.css";
+import logo from "../assets/zolu-logo.svg"; // <-- change to .png if that's your file
 
 export default function Header() {
   return (
-    <header>
-      <img src="/logo.png" alt="ZoLu Recruitment" />
-    </header>
-  );
-}
+    <header className="zolu-header">
+      <div className="zolu-header__inner">
+        {/* Brand */}
+        <a href="/" className="zolu-brand" aria-label="ZoLu Recruitment home">
+          <img src={logo} alt="ZoLu" className="zolu-brand__logo" />
+          <span className="zolu-brand__sub">Recruitment</span>
+        </a>
 
-        {/* Right: Primary nav */}
+        {/* Primary nav */}
         <nav className="zolu-nav" aria-label="Primary">
-          <NavLink to="/" end className="zolu-nav__link">
-            HOME
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              "zolu-nav__link" + (isActive ? " is-active" : "")
+            }
+          >
+            Home
           </NavLink>
-          <NavLink to="/services" className="zolu-nav__link">
-            FIND STAFF
+          <NavLink
+            to="/services"
+            className={({ isActive }) =>
+              "zolu-nav__link" + (isActive ? " is-active" : "")
+            }
+          >
+            Find Staff
           </NavLink>
           <NavLink
             to="/contact"
             className={({ isActive }) =>
-              `zolu-nav__link ${isActive ? "is-active" : ""}`
+              "zolu-nav__link" + (isActive ? " is-active" : "")
             }
           >
-            FIND WORK
+            Find Work
           </NavLink>
-          <NavLink to="/about" className="zolu-nav__link">
-            ABOUT
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              "zolu-nav__link" + (isActive ? " is-active" : "")
+            }
+          >
+            About
           </NavLink>
         </nav>
       </div>
